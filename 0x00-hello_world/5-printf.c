77b6167e3745c3204d912391f0844cac3367ae36
HopeAnagli <anaglihope481@gmail.com>
2023-02-20 11:23:05 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  main - it shows the main working space
+ *
+ *  Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	Return(0);
+}
