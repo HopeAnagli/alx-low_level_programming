c80a14b6054defc294f573e14e18c34d7a5bbbb9
HopeAnagli <anaglihope481@gmail.com>
2023-03-01 08:25:28 -0800

    message

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -22,11 +22,10 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (b > c && c > a)
 		largest = b;
 	else if (c > a && a > b)
 		largest = c;
-	else if (c > b && b > a)
+	else
 	{
 		largest = c;
 	}
