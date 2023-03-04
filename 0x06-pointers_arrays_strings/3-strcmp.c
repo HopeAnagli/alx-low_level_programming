1bfddc3b659b16cac1296a75f8008d255b1a53c9
HopeAnagli <anaglihope481@gmail.com>
2023-03-03 19:00:58 -0800

    whoop

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,21 @@
 #include "main.h"
-
 /**
- * _strcmp - comapre string
- * @s1: strings
- * @s2: strings
- * Return: 0
+ * _strcmp - compare
+ * @s1 : pointerto char params
+ * @s2 : pointer to char params
+ * Return: *dest
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int R;
 
-	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0' ; i++)
+	i = 0;
+
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		if (s1[i] != s2[i])
-		{
-			if (s1[i] < s2[i])
-				return (s1[i] - s2[i]);
-			else if (s1[i] > s2[i])
-				return (s1[i] - s2[i]);
-		}
-		else
-			return (0);
+		i++;
 	}
-	return (0);
+	R = s1[i] - s2[i];
+	return (R);
 }
